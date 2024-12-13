1644f0a51dc2f9c1c913ec9f3b16171a04790e75
Victor MORALES <victor.morales@outlook.fr>
2024-12-13 16:43:47 +0100

    setup routing

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+import PWABadge from '../PWABadge.tsx'
+import '../css/App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <PWABadge />
+    </>
+  )
+}
+
+export default App
